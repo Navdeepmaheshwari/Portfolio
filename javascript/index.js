a4a66d8923b9dfd7d40969a01214c79a9892ef9d
NAVDEEP MAHESHWARI <navdeepmaheshwari2002@gmail.com>
2022-03-21 12:22:22 +0530

    Update2

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,21 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 300) {
-      $(".navbar").css("background", "rgba(34, 34, 34)");
-    }
-    else{
-        $(".navbar").css("background" , "none");}
-  });
-});
+window.onscroll = () => {
+  const nav = document.getElementById("navb");
+  if (this.scrollY <= 10)
+    document.getElementById("navb").classList.remove("scroll");
+  else document.getElementById("navb").classList.add("scroll");
+};
+function myEdu() {
+  document.getElementById("education").style.display = "block";
+  document.getElementById("experience").style.display = "none";
+  document.getElementById("eduheading").innerText = "Education";
+  document.getElementById("edbtn").classList.add("selected");
+  document.getElementById("exbtn").classList.remove("selected");
+}
+
+function myExp() {
+  document.getElementById("education").style.display = "none";
+  document.getElementById("experience").style.display = "block";
+  document.getElementById("eduheading").innerText = "Experience";
+  document.getElementById("exbtn").classList.add("selected");
+  document.getElementById("edbtn").classList.remove("selected");
+}
